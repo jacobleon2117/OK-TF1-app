c70bc5a9c2130c50e9278af00b102d714323ace5
jacobleon2117 <jacobleon2117@gmail.com>
2025-04-06 16:20:44 -0500

    fixing typo

--- App.tsx
+++ App.tsx
@@ -46,3 +46,20 @@ const AppNavigator = () => {
       </View>
     );
   }
+
+  return (
+    <NavigationContainer>
+      {user ? <DashboardNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default AppNavigator;
