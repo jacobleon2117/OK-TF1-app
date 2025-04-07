66e188b44fa77c54a2d0677479729dba28981952
jacobleon2117 <jacobleon2117@gmail.com>
2025-04-06 22:42:44 -0500

    getting rid of jest....

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,55 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./"],
-          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            "@": "./src",
-            "@components": "./src/components",
-            "@screens": "./src/screens",
-            "@navigation": "./src/navigation",
-            "@store": "./src/store",
-            "@utils": "./src/utils",
-            "@assets": "./assets",
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@store': './src/store',
+            '@utils': './src/utils',
+            '@assets': './assets',
           },
         },
       ],
-      "module:react-native-dotenv"
-    ]
+      'module:react-native-dotenv',
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+      test: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: { node: 'current' },
+              modules: 'commonjs',
+            },
+          ],
+          'babel-preset-expo',
+        ],
+        plugins: [
+          '@babel/plugin-transform-modules-commonjs',
+          ['@babel/plugin-proposal-class-properties', { loose: true }],
+          [
+            '@babel/plugin-transform-typescript',
+            {
+              allowDeclareFields: true,
+              isTSX: true,
+              allExtensions: true,
+            },
+          ],
+        ],
+      },
+    },
   };
 };
--- src/__tests__/smoke.test.ts
+++
@@ -1,6 +0,0 @@
-// src/__tests__/smoke.test.ts
-describe('Smoke Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
