4896370ebaa1d484e3c560c364466c716a0d5f91
Jacob Leon <143765559+jacobleon2117@users.noreply.github.com>
2025-04-06 23:04:54 -0500

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { 
-  FIREBASE_API_KEY, 
+
+import {
+  FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID 
-} from '@env';
+  FIREBASE_APP_ID,
+} from 'react-native-dotenv';
 
-// Firebase configuration object
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -20,13 +20,8 @@ const firebaseConfig = {
   appId: FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Auth 
 const auth = getAuth(app);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { app, auth, db };
